8ad4b2370c2106d0326502a60d6e1faba377a1de
Glitch-Sound <uid.glitch@gmail.com>
2024-10-29 08:32:36 +0900

    frontend

--- frontend/app/glitch/src/components/dialog/LoginDialog.vue
+++ frontend/app/glitch/src/components/dialog/LoginDialog.vue
@@ -32,6 +32,7 @@ defineExpose({
             v-model="form_data.user"
             :rules="[rules.required, rules.alphanumeric]"
             label="User"
+            autocomplete="current-user"
             required
           />
 
@@ -40,6 +41,7 @@ defineExpose({
             :rules="[rules.required, rules.password]"
             label="Password"
             type="password"
+            autocomplete="current-password"
             required
           />
         </v-form>
